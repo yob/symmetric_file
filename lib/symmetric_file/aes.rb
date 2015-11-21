a9cec4669a19a2cb8ed309965263f513e269cbaf
James Healy <james@yob.id.au>
2015-11-21 21:47:59 +1100

    alter file format
    * Use v3 of the RNCryptor data format [1]
    * Use PBKDF2 to convert passphrases into a 256bit key
    * Use a different key for the ciphertext and HMAC
    * There's a ruby implementation of RNCryptor [2], but it's simple enough
      that I'd rather re-implement it instead of depending on a gem
    
    [1] https://github.com/RNCryptor/RNCryptor
    [2] https://rubygems.org/gems/ruby_rncryptor

--- lib/symmetric_file/aes.rb
+++ lib/symmetric_file/aes.rb
@@ -1,11 +1,12 @@
 require 'openssl'
 require 'digest/sha2'
+require 'securerandom'
 require 'base64'
 
 module SymmetricFile
   # Wrapper for openssl for encrypting and decrypting with AES-256, a symmetric cypher
   # that uses the same key for both encryption and decryption.
-  # 
+  #
   class Aes
     SEPERATOR = "--"
 
@@ -14,29 +15,49 @@ module SymmetricFile
     end
 
     def encrypt(input)
+      version = 3.chr
+      options = 1.chr # uses password
+      encryption_salt = SecureRandom.random_bytes(8)
+      hmac_salt = SecureRandom.random_bytes(8)
+      hmac_key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(@key , hmac_salt, 10000, 32)
+
       cipher = OpenSSL::Cipher.new('aes-256-cbc')
       cipher.encrypt
-      cipher.key = @key
+      cipher.key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(@key, encryption_salt, 10000, 32)
       iv = cipher.random_iv
       ciphertext = cipher.update(input.to_s) + cipher.final
-      cipher_msg = [encode64(iv), encode64(ciphertext)].join(SEPERATOR)
-      hmac = calc_hmac(cipher_msg)
-      [encode64(iv), encode64(ciphertext), encode64(hmac)].join(SEPERATOR)
+      msg = version + options + encryption_salt + hmac_salt + iv + ciphertext
+      hmac = [OpenSSL::HMAC.hexdigest('sha256', hmac_key, msg)].pack('H*')
+      msg + hmac
     end
 
-
     def decrypt(input)
-      iv64, ciphertext64, hmac64 = input.to_s.split(SEPERATOR)
-      if iv64.nil? || ciphertext64.nil? || hmac64.nil?
-        raise InputError, "Input not a recogised format"
-      elsif calc_hmac(iv64 + SEPERATOR + ciphertext64) != decode64(hmac64)
+      raise InputError, "Encrypted data too short" unless input.bytesize >= 67
+
+      version         = input[0,1]
+      options         = input[1,1]
+      encryption_salt = input[2,8]
+      hmac_salt       = input[10,8]
+      iv              = input[18,16]
+      ciphertext      = input[34, input.bytesize-66]
+      hmac            = input[-32, 32]
+
+      raise InputError, "Only version 3 data can be decrypted" unless version == "\x03"
+
+      msg = version + options + encryption_salt + hmac_salt + iv + ciphertext
+
+      hmac_key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(@key, hmac_salt, 10000, 32)
+      verified = [OpenSSL::HMAC.hexdigest('sha256', hmac_key, msg)].pack('H*') == hmac
+
+      unless verified
         raise InputError, "HMAC validation failed. The passphrase may be incorrect or the encrypted message may have been tampered with"
       end
+
       cipher = OpenSSL::Cipher.new('aes-256-cbc')
       cipher.decrypt
-      cipher.key = @key
-      cipher.iv = decode64(iv64)
-      cipher.update(decode64(ciphertext64)) + cipher.final
+      cipher.key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(@key, encryption_salt, 10000, 32)
+      cipher.iv = iv
+      cipher.update(ciphertext) + cipher.final
     end
 
     private
